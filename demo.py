3f99a1bfd8cf31178709e0f8ef5fd6731376e89d
cwhite911 <smortopahri@gmail.com>
2019-02-11 20:31:03 -0500

    Added sample python script

--- demo.py
+++ demo.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+
 import grass.script as gscript
 
 def main():
+    #Location: nc_spm_08_grass7
     #Run Hand Methodology with 3m inundation at 0.5m intervals
     gscript.run_command("r.watershed", elevation="elevation", accumulation="flowacc", drainage="drainage", stream="streams", threshold=100000, overwrite=True)
     gscript.run_command("r.to.vect", input="streams", output="streams", type="line", overwrite=True)
